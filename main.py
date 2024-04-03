aaea8ba29b60aaf42a52825c85ecfa692e130e8b
XL <16784876+XiaoLiu8811@users.noreply.github.com>
2024-04-02 23:19:36 -0400

    Update main.py
    Adding the search button. So that the customers could search their account and password for the desired website.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -32,19 +33,61 @@ def save():
     website = web_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title= "Oops", message="Please don't leave any fields empty!")
     elif len(password) < 8:
         messagebox.showinfo(title= "Oops", message="Password should have more than 8 characters!")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"There are the details entered: \nEmail: {email} "
-                                                      f"\nPassword: {password} \nIs it ok to save?")
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+
+        finally:
             web_entry.delete(0, END)
             password_entry.delete(0, END)
+
+# ---------------------------- Search ------------------------------- #
+def search_data():
+    website = web_entry.get()
+    try:
+        with open("data.json", "r") as data_file:
+            # Reading old data
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for the {website} exists.")
+
+        # When if-else statements can address the problem don't use the try-except-else method
+        # try:
+        #     messagebox.showinfo(title=website, message=f"Email: {data[website]["email"]}\n"
+        #                                                f"Password: {data[website]["password"]}")
+        # except KeyError:
+        #     messagebox.showinfo(title="Error", message=f"No details for the {website} exists.")
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -65,14 +108,17 @@ add_button.grid(column=1, row=4, sticky="w")
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=1, sticky="E")
 
+search_button = Button(text="Search", width=8, command=search_data)
+search_button.grid(row=1, column=1, sticky="E")
+
 # Entries
 
-web_entry = Entry(width=42)
+web_entry = Entry(width=30)
 web_entry.grid(column=1, row=1, sticky="w")
 web_entry.focus()
 email_entry = Entry(width=42)
 email_entry.grid(column=1, row=2, sticky="w")
-email_entry.insert(0, "your@gmail.com")
+email_entry.insert(0, "username@gmail.com")
 password_entry = Entry(width=23)
 password_entry.grid(column=1, row=3, sticky="w")
 
